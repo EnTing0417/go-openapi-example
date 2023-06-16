b62eef39b2243b64b06c279d015170cd153fd14d
enting <enting@enting.ting>
2023-06-16 03:34:13 -0500

    code clean

--- README.md
+++ README.md
@@ -3,10 +3,31 @@
 # pre-requisite:
 - Install go
 
-# to run the program
+# run the program
 execute "go run main.go"
 
-execute "swag init" whenever make changes to swagger.yaml 
-
-# to access swagger
+# access swagger
 http://localhost:8080/swagger/index.html
+
+
+# add new api
+
+STEP 1 - Add new code snippet in main.go,
+
+// @BasePath /api/v1
+// PingExample godoc
+// @Summary ping example
+// @Schemes
+// @Description do ping
+// @Tags example
+// @Accept json
+// @Produce json
+// @Success 200 {string} Helloworld
+// @Router /helloworld [get]
+func Helloworld(g *gin.Context)  {
+	g.JSON(http.StatusOK,"helloworld")
+ }
+
+STEP 2 - execute "swag init" in the root directory /go-swagger-example
+
+
--- main.go
+++ main.go
@@ -3,31 +3,17 @@ package main
 import (
    "github.com/gin-gonic/gin"
    docs "github.com/go-swagger-example/docs"
+   api "github.com/go-swagger-example/api"
    swaggerfiles "github.com/swaggo/files"
    ginSwagger "github.com/swaggo/gin-swagger"
-   "net/http"
 )
-// @BasePath /api/v1
-
-// PingExample godoc
-// @Summary ping example
-// @Schemes
-// @Description do ping
-// @Tags example
-// @Accept json
-// @Produce json
-// @Success 200 {string} Helloworld
-// @Router /helloworld [get]
-func Helloworld(g *gin.Context)  {
-   g.JSON(http.StatusOK,"helloworld")
-}
 
 func main()  {
    r := gin.Default()
    docs.SwaggerInfo.BasePath = "/api/v1"
    v1 := r.Group("/api/v1")
    {
-    	v1.GET("/helloworld",Helloworld)
+    	v1.GET("/helloworld",api.Helloworld)
    }
    r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerfiles.Handler))
    r.Run(":8080")
